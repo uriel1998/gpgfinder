a80edec5c58e3708a413bc82c4699c9d7610f3c0
Steven Saus <steven.saus@gmail.com>
2017-03-25 23:45:24 -0400

    added csv awk script (public domain) and tests for google csv parsing

--- csvparser.sh
+++ csvparser.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# csv.awk from http://lorance.freeshell.org/csv/ where it's in the
+# public domain
+
 # Reference to counting characters 
 # http://stackoverflow.com/questions/16679369/count-occurrences-of-char-in-string-using-bash
 
@@ -7,34 +10,42 @@
 	#file -i google.csv
 	#iconv -f UTF16LE google.csv -o googleout.csv
 	
-	# if argv < 2, assign divider as , otherwise use $2
+	#default divider is a comma for CSV
 	div=,
 	
-	# test for file with $1
 	
-	
+	if [ ! -f "$1" ]; then
+		exit
+	fi
+ 	
 	#header row - number of columns there should be.
 	numcols=$(head -n 1 $1 | grep -o $div | wc -l)
 	echo $numcols
-	numcols=$numcols-$(head -n 1 $1 | grep -o \" | wc -l)
-	read
+	if [ $numcols = 86 ];then
+		CSVType=$(head -n 1 $1 | awk -f csv.awk | awk -F"|"  '{print $30}')
+		if [ "$CSVType" == "E-mail 1 - Value" ];then
+			echo "Google Contacts CSV export detected"
+		fi
+	fi
+
+	
+
+# column 1 and 29 for Google Contacts CSV export
 	while read line; do
 		str=$(echo "$line")
 		linecols=$(echo $str | grep -o $div | wc -l)
-		while [ "$linecols" -lt "$numcols" ]; do
-		#if [ "$linecols" != "$numcols" ]; then
-			read line
-			str=$(echo $str $line)
-			linecols=$(echo $str | grep -o $div | wc -l)
+		#echo "$linecols"
+		#echo "$str"
+		if [ $linecols == $numcols ] || [[ $str =~ ^\" ]];then
+			Name=$(echo "$line" | awk -f csv.awk | awk -F"|"  '{print $2}')
+			Email=$(echo "$line" | awk -f csv.awk | awk -F"|"  '{print $30}')	
+			# Testing for header line
+			if [ "$Name" != "Name" ]; then			
+				if [ "$Email" != "" ]; then
+				echo "$Name"
+				echo "$Email"
+				fi
+			fi
+		fi
 
-
-			# how many cols
-
-			
-			echo "$linecols $numcols BOOGER"
-			
-			
-			#echo $line | grep -c \"
-		done
-		echo "$str"
 	done <$1
